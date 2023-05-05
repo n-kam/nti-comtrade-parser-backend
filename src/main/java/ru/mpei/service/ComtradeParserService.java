dc64fa900ab1c7db1e716ae26f77afba1cc1a62e
n-kam <>
2023-05-05 23:02:21 +0300

    fix and cleanup ComtradeParserService

--- src/main/java/ru/mpei/service/ComtradeParserService.java
+++ src/main/java/ru/mpei/service/ComtradeParserService.java
@@ -1,6 +1,5 @@
 package ru.mpei.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.mpei.dto.ComtradeDto;
 
@@ -8,34 +7,23 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
 public class ComtradeParserService {
 
-    @Value("${cfgFileName}")
-    private String cfgFileName;
-    @Value("${datFileName}")
-    private String datFileName;
-    @Value("${current}")
-    private String current;
+    public ComtradeDto parse(Map<String, String> comtradeFile) {
 
-    public ComtradeDto parse() {
-
-
-        cfgFileName = "comtrade/Number start = 698 Test = 4.2.1.1 Time = 07_19_2022 13_54_51.673 RTDS.cfg";
-        datFileName = "comtrade/Number start = 698 Test = 4.2.1.1 Time = 07_19_2022 13_54_51.673 RTDS.dat";
-
+        String cfgFileName = comtradeFile.get("cfg");
+        String datFileName = comtradeFile.get("dat");
 
         ComtradeDto comtradeDto = new ComtradeDto();
         Iterator<String> linesIterator;
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(cfgFileName))));
+
         linesIterator = reader.lines().toList().iterator();
 
         String[] line = linesIterator.next().split(" *, *");
@@ -50,31 +38,9 @@ public class ComtradeParserService {
         int discreteChannelCount = Integer.parseInt(line[2].replaceAll("[a-zA-Z]", ""));
         comtradeDto.setDiscreteChannelCount(discreteChannelCount);
 
-//        List<ComtradeDto.AnalogChannel> analogChannels = new ArrayList<>();
-//        List<ComtradeDto.DiscreteChannel> discreteChannels = new ArrayList<>();
         List<ComtradeDto.Channel> analogChannels = new ArrayList<>();
         List<ComtradeDto.Channel> discreteChannels = new ArrayList<>();
 
-//        for (int i = 0; i < analogChannelCount; i++) {
-//
-//            line = linesIterator.next().split(" *, *");
-//            analogChannels.add(new ComtradeDto.AnalogChannel(
-//                    Integer.parseInt(line[0]),
-//                    line[1],
-//                    line[2],
-//                    line[3],
-//                    line[4],
-//                    Double.parseDouble(line[5]),
-//                    Double.parseDouble(line[6]),
-//                    Double.parseDouble(line[7]),
-//                    Double.parseDouble(line[8]),
-//                    Double.parseDouble(line[9]),
-//                    Double.parseDouble(line[10]),
-//                    Double.parseDouble(line[11]),
-//                    line[12],
-//                    new ArrayList<>()));
-//
-//        }
 
         for (int i = 0; i < analogChannelCount; i++) {
 
@@ -98,18 +64,6 @@ public class ComtradeParserService {
                     new ArrayList<>()));
         }
 
-//        for (int i = 0; i < discreteChannelCount; i++) {
-//            line = linesIterator.next().split(" *, *");
-//
-//            discreteChannels.add(new ComtradeDto.DiscreteChannel(
-//                    Integer.parseInt(line[0]),
-//                    line[1],
-//                    line[2],
-//                    line[3],
-//                    Short.parseShort(line[4]),
-//                    new ArrayList<>()));
-//
-//        }
 
         for (int i = 0; i < discreteChannelCount; i++) {
             line = linesIterator.next().split(" *, *");
@@ -134,10 +88,6 @@ public class ComtradeParserService {
 
         }
 
-//        comtradeDto.setAnalogChannels(analogChannels);
-//        comtradeDto.setDiscreteChannels(discreteChannels);
-
-
         comtradeDto.setFreq(Double.parseDouble(linesIterator.next()));
         comtradeDto.setNRates(Integer.parseInt(linesIterator.next()));
         line = linesIterator.next().split(" *, *");
@@ -155,7 +105,6 @@ public class ComtradeParserService {
         comtradeDto.setFileType(linesIterator.next());
         comtradeDto.setTimeMultiplier(Double.parseDouble(linesIterator.next()));
 
-
         reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(datFileName))));
         linesIterator = reader.lines().toList().iterator();
 
@@ -163,17 +112,6 @@ public class ComtradeParserService {
             line = linesIterator.next().split(" *, *");
 
             int i = 1;
-
-//            for (ComtradeDto.AnalogChannel analogChannel : analogChannels) {
-//                Double reading = Double.parseDouble(line[i]) * analogChannel.getA() + analogChannel.getB();
-//                analogChannel.getReadings().add(reading);
-//                i++;
-//            }
-//
-//            for (ComtradeDto.DiscreteChannel discreteChannel : discreteChannels) {
-//                discreteChannel.getReadings().add(Integer.parseInt(line[i]));
-//                i++;
-//            }
 
             for (ComtradeDto.Channel analogChannel : analogChannels) {
                 Double reading = Double.parseDouble(line[i]) * analogChannel.getA() + analogChannel.getB();
